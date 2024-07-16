99c4d1038e4659014cee91393c2496d152633b0c
DarshanSp <119423403+Darshan-S-P@users.noreply.github.com>
2024-07-16 22:52:21 +0530

    Folder:commit

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './TitleCards.css'
+const TitleCards = () => {
+  return (
+    <div className='titlecards'>
+      
+    </div>
+  )
+}
+
+export default TitleCards
---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Player.css'
+const Player = () => {
+  return (
+    <div className='player'>
+      
+    </div>
+  )
+}
+
+export default Player
