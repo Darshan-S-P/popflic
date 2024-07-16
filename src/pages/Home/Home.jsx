ac4e23eef70f40cdb8533994701b2c14dc491561
DarshanSp <119423403+Darshan-S-P@users.noreply.github.com>
2024-07-16 23:57:44 +0530

    navbar-fomt:commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Home.css'
-
+import Navbar from '../../components/Navbar/Navbar'
 const Home = () => {
   return (
     <div className='home'>
-      
+      <Navbar/>
     </div>
   )
 }
